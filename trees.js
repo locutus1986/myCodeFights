8cf18639428efbdbdd0e868354f67d7a4e5fe5c3
locutus1986 <locutus1986@gmail.com>
2018-05-01 16:43:11 -0400

    fixed formatting

--- trees.js
+++ trees.js
@@ -1,9 +1,14 @@
 /*  The problem
-Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
+Some people are standing in a row in a park.
+There are trees between them which cannot be moved.
+Your task is to rearrange the people by their heights in
+a non-descending order without moving the trees.
 
 Example
 
-For a = [-1, 150, 190, 170, -1, -1, 160, 180], the output should be
+For a = [-1, 150, 190, 170, -1, -1, 160, 180], the output
+should be
+
 sortByHeight(a) = [-1, 150, 160, 170, -1, -1, 180, 190].
 
 Input/Output
@@ -12,7 +17,9 @@ Input/Output
 
 [input] array.integer a
 
-If a[i] = -1, then the ith position is occupied by a tree. Otherwise a[i] is the height of a person standing in the ith position.
+If a[i] = -1, then the ith position is occupied by a tree.
+Otherwise a[i] is the height of a person standing in the ith
+position.
 
 Guaranteed constraints:
 5 ≤ a.length ≤ 15,
@@ -51,10 +58,8 @@ function sortByHeight(a) {
   }
  console.log(trees);
  console.log(a);
+ return a
 }
 
-function compare(a, b){
-  return b-a;
-}
 const a = [4, 2, 9, 11, 2, 16];
 sortByHeight(a);
